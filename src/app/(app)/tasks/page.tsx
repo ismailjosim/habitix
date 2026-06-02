287e3a7bb23cd5d0d77f309b15e7bd58b93acfa0
ismailjosim <ismailjosim99@gmail.com>
2026-06-02 09:28:40 +0600

    Restricted assignment-detail updates to the assigner/admin and notify recipients on updates in tasks.ts

--- src/app/(app)/tasks/page.tsx
+++ src/app/(app)/tasks/page.tsx
@@ -3,11 +3,27 @@ import { LAYOUT_CONSTRAINTS } from '@/lib/layout-constraints';
 import { getTaskBoardData } from '@/lib/queries/tasks';
 
 export default async function TasksPage() {
-  const { personalTasks, assignedTasks } = await getTaskBoardData();
+  const {
+    personalTasks,
+    assignedTasks,
+    assignableStudents,
+    assignableTeams,
+    currentRole,
+    currentProfileId,
+    canAssignTasks,
+  } = await getTaskBoardData();
 
   return (
     <div className={`${LAYOUT_CONSTRAINTS.pageMaxWidth} ${LAYOUT_CONSTRAINTS.pagePadding} mx-auto`}>
-      <TaskBoard personalTasks={personalTasks} assignedTasks={assignedTasks} />
+      <TaskBoard
+        personalTasks={personalTasks}
+        assignedTasks={assignedTasks}
+        assignableStudents={assignableStudents}
+        assignableTeams={assignableTeams}
+        currentRole={currentRole}
+        currentProfileId={currentProfileId}
+        canAssignTasks={canAssignTasks}
+      />
     </div>
   );
 }
